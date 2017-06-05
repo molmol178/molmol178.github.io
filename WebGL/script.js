2e2bcff50c15dac8d5be0b23b9fde4e440bc9561
molmol178 <kazuto.molmol@gmail.com>
2017-06-06 01:59:31 +0900

    color and animation

--- WebGL/script.js
+++ WebGL/script.js
@@ -15,10 +15,14 @@ onload = function(){
 
     var prg = create_program(v_shader, f_shader);
 
-    var attLocation = gl.getAttribLocation(prg, 'position');
+    var attLocation = new Array(2);
+    attLocation[0] = gl.getAttribLocation(prg, 'position');
+    attLocation[1] = gl.getAttribLocation(prg, 'color');
 
     //attributeの要素数（この場合xyzの三要素）
-    var attStride = 3;
+    var attStride = Array(2);
+    attStride[0] = 3;
+    attStride[1] = 4;
 
     var vertex_position = [
         0.0, 1.0, 0.0,
@@ -26,33 +30,72 @@ onload = function(){
         -1.0, 0.0, 0.0
     ];
 
-    var vbo = create_vbo(vertex_position);
+    var vertex_color = [
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0
+    ];
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    gl.enableVertexAttribArray(attLocation);
-    gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
+    var position_vbo = create_vbo(vertex_position);
+    var color_vbo = create_vbo(vertex_color);
+
+    set_attribute([position_vbo, color_vbo], attLocation, attStride);
+    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
+
 
     var m = new matIV();
 
     var mMatrix = m.identity(m.create());
     var vMatrix = m.identity(m.create());
     var pMatrix = m.identity(m.create());
+    var tmpMatrix = m.identity(m.create());
     var mvpMatrix = m.identity(m.create());
 
     //ビュー座標変換行列
-    m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
-
+    m.lookAt([0.0, 0.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
     //プロジェクション変換行列
     m.perspective(90, canvas.width / canvas.height, 0.1, 100, pMatrix);
-
     //各行列をかけ合わせて座標変換行列を完成させる
-    m.multiply(pMatrix, vMatrix, mvpMatrix);
-    m.multiply(mvpMatrix, mMatrix, mvpMatrix);
+    m.multiply(pMatrix, vMatrix, tmpMatrix);
+
+    var count = 0;
+(function(){
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    count++;
+    var rad = (count % 360) * Math.PI / 180;
+    var x = Math.cos(rad);
+    var y = Math.sin(rad);
     
-    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
+    //一つ目
+    m.identity(mMatrix);
+    m.translate(mMatrix, [x, y + 1.0, 0.0], mMatrix);
+    m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    //二つ目
+    m.identity(mMatrix);
+    m.translate(mMatrix, [1.0, -1.0, 0.0], mMatrix);
+    m.rotate(mMatrix, rad, [0, 1, 0], mMatrix);
+
+    m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    //３つめ
+    var s = Math.sin(rad) + 1.0;
+    m.identity(mMatrix);
+    m.translate(mMatrix, [-1.0, -1.0, 0.0], mMatrix);
+    m.scale(mMatrix, [s, s, 0.0], mMatrix);
+    m.multiply(tmpMatrix, mMatrix, mvpMatrix);
     gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
     gl.drawArrays(gl.TRIANGLES, 0, 3);
     gl.flush();
+
+    setTimeout(arguments.callee, 1000 / 30);
+})();
 
     //シェーダを生成，コンパイルする関数
     function create_shader(id){
@@ -68,7 +111,7 @@ onload = function(){
             case 'x-shader/x-fragment':
                 shader = gl.createShader(gl.FRAGMENT_SHADER);
                 break;
-            defalt:
+            default:
                 return;
         }
 
@@ -99,12 +142,27 @@ onload = function(){
 
     }
 
-    //VBOを生成する関数
-    function create_vbo(data){
+   function create_vbo(data){
         var vbo = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
         return vbo;
+    }
+
+    function set_attribute(vbo, attL, attS){
+        for(var i in vbo){
+            gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
+            gl.enableVertexAttribArray(attL[i]);
+            gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+        }
+    }
+
+    function create_ibo(data){
+        var ibo = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+        return ibo;
     }
 };
