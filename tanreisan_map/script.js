affcbe9f7eaf36609566277ee9791f1184094216
molmol178 <kazuto.molmol@gmail.com>
2019-03-28 20:37:13 +0900

    fix hukidashi view

--- tanreisan_map/script.js
+++ tanreisan_map/script.js
@@ -49,11 +49,11 @@ function addMarker(marker, data, pin_color){
         , {title: data.shopname}
     );
     marker.bindInfoWindow('\
-        <div style="word-wrap: break-word; white-space:pre-wrap; overflow-scrolling : touch; -webkit-overflow-scrolling: touch;">\
         <img src='+ data.display_url +' width=50%><br>\
-        2019/3/25時点でのtanreisanのLike数：'+ data.edge_media_preview_like.count + '<br>\
-        ' + data.text + '</div>\
-    ');
+        2019/3/25時点でのtanreisanのLike数：'+ data.edge_media_preview_like.count + '<br><br>\
+        '+ data.shopname + '<br>\
+        <a href="https://www.instagram.com/p/' + data.shortcode + '">tanreisanの投稿へ</a><br>\
+         ');
     return marker;
 }
 
